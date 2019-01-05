963d0c4a7b07ccbe6f4a0271c519187453b80cf8
Young Hyeon <44453931+yh0319@users.noreply.github.com>
2019-01-05 16:36:23 +0900

    디자인 추가 및 데이터셋 추가
    비동기 처리 및 UI 수정

--- django/mysite/country/admin.py
+++ django/mysite/country/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from country.models import dong_data
+from country.models import Dong_data, KangwonCity, Infra
 # Register your models here.
 
-admin.site.register(dong_data)
+admin.site.register(Dong_data)
+admin.site.register(KangwonCity)
+admin.site.register(Infra)
--- django/mysite/country/views.py
+++ django/mysite/country/views.py
@@ -3,54 +3,51 @@ from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.shortcuts import render
 import json
-from django.db.models import Q
+
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-from .models import dong_data
-    
+
+from country.func import sortCity
+from country.models import KangwonCity, Infra, Dong_data
+
 
 # 인덱스 화면을 띄우는 함수
 def index(request):
     print("---------index()-------------")
+    city_list = KangwonCity.objects.all()
+    print(city_list)
+    context = {'city_list': city_list}
+    return render(request, 'country/index.html', context)
 
-    return render(request, 'country/index.html')
 
-"""
-def detail(request):
-    print("---------detail()-------------")
-    dong = dong_data.objects.all()
-    dong = dong.filter(Q(city='강릉시') | Q(city='동해시') | Q(city='춘천시'))
-    context = {'dong':dong}
-    return render(request, 'country/detail.html', context)
-"""
 
-def detail(request):
-    print("---------detail()-------------")
+# 인덱스에서 비동기로 값 넘기기
+def searching(request):
+    print("------searching()-------")
     try:
-        form = request.POST['selectedCity']
-        print('입력된 값 : ', form)
-        cityList = form.split(',')
-        print('변환된 값 : ', cityList)
-
+         form = request.POST['data']
+         print('입력된 값 : ', form)
+         cityList = form.split(',')
+         print('변환된 값 : ', cityList)
     except(KeyError):
-        return render(request, 'country/detail.html',{'error_message' : '값 오류입니다.'} )
+        
+         return render(request, {'error_message' : '값 오류입니다.'} )
     
-    args = {'cityList':cityList}
-
-    #dong = dong_data.objects.all()
-    #dong = dong.filter(city==cityList)
-    #context = {'dong':dong}
-
-    # 받아온 지역이름에 해당하는 데이터만 갖고오는 sortCity()함수 호출
-    dong = dong_data.objects.all()
+    dong = Dong_data.objects.all()
     dong = dong.filter(city__in= cityList)
-    context = {'dong':dong}
+    context = {'dong': dong}
     global testtest
+    
     testtest = dong
-    print("globaltext" , testtest)
-    return render(request, 'country/detail.html', context)
-    # request.session['selectedCity'] = cityList
+    print("global 데이터" , testtest)
+    return HttpResponse(request, content_type="text/plain")
+
+
+
+
+
+# 디테일 화면에서 값 입력 받고 결과창으로 넘기는 함수
 
 
 def result(request):
@@ -66,21 +63,6 @@ def result(request):
     print('입력된 3순위 : ', rank3)
 
     dong = testtest.order_by(rank1, rank2, rank3)[:10]
-
-    print("dong data print : ",dong)
-
     context = {'dong':dong}
-
     # return render(request, 'country/result.html')
     return render(request, 'country/result.html', context)
-
-
-
-
-# 인덱스 화면에서 값 입력 받고 시별로 정렬 한 후 디테일로 넘기는 함수(세션 유지)
-
-
-# 디테일 화면에서 값 입력 받고 결과창으로 넘기는 함수
-
-
-# 로직 함수 결과 받아서 결과 창에서 지도로 시각화해 보여주는 함수
