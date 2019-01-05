a3f37a7d1c8c74d89c348538518b9ed6c82a92d1
kimdom194 <41944346+kimdom194@users.noreply.github.com>
2019-01-05 14:58:28 +0900

    Django
    01_05

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'country/templates/country')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# 추가 설정할 경우 css 수정 사항이 실시간 적용됨
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
