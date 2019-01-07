ace5ed3af4a6fbb780ccbf50463a7d8265b86376
khk376 <khk376@naver.com>
2019-01-07 17:03:24 +0900

    result page 수정

--- django/mysite/country/models.py
+++ django/mysite/country/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Dong_data(models.Model):
     dong = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    medical = models.CharField(max_length=100)
-    school = models.CharField(max_length=100)
-    house = models.CharField(max_length=100)
+    medical = models.IntegerField(max_length=100)
+    school = models.IntegerField(max_length=100)
+    house = models.IntegerField(max_length=100)
 
     def __str__(self):
         return self.dong
